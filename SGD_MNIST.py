136e627f276de307afaf9e60837ea5b76b9b8bd2
Sjahriyar <sjahriyar.soheytizadeh@ns.nl>
2025-03-20 15:15:17 +0100

    update notebook, rename files

--- SGD_MNIST.py
+++ SGD_MNIST.py
@@ -28,43 +28,19 @@ test_dataloader = DataLoader(test_data, batch_size=batch_size)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
 
-# This code defines a neural network with three fully connected (Linear) layers and two activation functions (ReLU). 
-# The layers are applied sequentially, meaning the output of one layer becomes the input to the next.
 class NeuralNetwork(nn.Module):
   def __init__(self):
     super().__init__()
     # same as `.view(-1, 28*28)` it flattens the input tensor to 1D tensor
     self.flatten = nn.Flatten()
-    # Randomly initialize weights
-    # nn.Sequential is a container for stacking layers in sequence.
     self.linear_relu_stack = nn.Sequential(
-      # 28x28 means how many features our input has? 28*28 = 784
-      # In our 28x28 image, we have 784 features
-      # 512: The number of neurons (or units) in this layer. Each neuron computes a weighted sum of the inputs and applies a bias.
-      # For more information, see the optional reads: "Number of neurons (or units) in the hidden layers" section.
-      # Why using Linear layer? READ: optinal_read.ipynb#linear-model
-      # The weights are part of the nn.Linear layers in your model. 
-      # Each nn.Linear layer creates its own set of weights and biases when it is initialized.
       nn.Linear(28*28, 512),
-
-      # ReLU is the activation function. It introduces non-linearity into the model.
-      # The ReLU activation function is applied element-wise to the output of the first layer, setting all negative values to 0, and leaving all other values unchanged.
-      # ReLU(x)=max(0,x)
       nn.ReLU(),
-
-      # The second fully connected layer accepts the output of the first layer as input, applies a bias, and outputs a 512-dimensional tensor.
       nn.Linear(512, 512),
       nn.ReLU(),
-
-      # The final fully connected layer receives the 512-dimensional tensor from the second layer and outputs a 10-dimensional tensor.
-      # 10 because our dataset has 10 classes.
-      # FashionMNIST has 10 classes (e.g. 0: T-shirt, 1: Trouser, 2: Pullover) so we have 10 output channels.
       nn.Linear(512, 10),
     )
 
-  # The forward function defines how input data (x) flows through the neural network to produce an output. 
-  # It specifies the sequence of operations that transform the input into predictions (logits). 
-  # This function is called automatically when you pass data through the model (e.g., model(x)).
   def forward(self, x):
     x = self.flatten(x)                 # Step 1: Flatten the input
     logits = self.linear_relu_stack(x)  # Step 2: Pass through the layers
@@ -107,11 +83,7 @@ def test(dataloader, model, loss_fn):
 
 model = NeuralNetwork().to(device)
 
-# To train a model we need a loss function and an optimizer
-
 loss_fn = nn.CrossEntropyLoss()
-# 1e-3 means that the optimizer will adjust the model parameters by the factor of 1e-3 (0.001) times the gradient.
-# This is a hyperparameter that we can tune.
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
 epochs = 5
